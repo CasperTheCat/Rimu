9d277855e9117794d3f1f8553a45de4e8f38c2e9
Chris Anderson <zsmyna@gmail.com>
2019-03-04 00:41:36 +1300

    Updated Rimu.py to include manual ordering

--- Rimu.py
+++ Rimu.py
@@ -3,28 +3,67 @@ import sys
 import pypandoc
 import codecs
 
-chapterMisc = []
+#chapterMisc = []
+#
+#for r,ds,fs in os.walk("./"):
+#    for f in fs:
+#        if(f.startswith("Chapter")):
+#            chapterMisc.append(r+f)
+#
+#print(chapterMisc)
+#
+#chapterMisc.sort()
+#
+#print(chapterMisc)
 
-for r,ds,fs in os.walk("./"):
-    for f in fs:
-        if(f.startswith("Chapter")):
-            chapterMisc.append(r+f)
+def ParseBeatStructure(x):
+    chapterList = []
+    print("Chapter Outline")
+    rollingChapterCounter = 0
+    for i in x:
+        i = i.rstrip()
+        i = i.lstrip()
+        if(i.startswith("# Arc")):
+            print(i[2:len(i)])
+            rollingChapterCounter = 0
+            continue
+        elif(i.startswith("- ")):
+            rollingChapterCounter += 1
+            f = i.split(" ")
+            if(len(f) == 3):
+                print("Chapter " + str(rollingChapterCounter) + " - \"" + f[1] + "\"")
+                chapterList.append(f[2][0:len(i)])
+            else:
+                print("Chapter " + str(rollingChapterCounter) + " - \"" + f[1] + "\" is not yet written!")
+    print(chapterList)
+    return chapterList
 
-print(chapterMisc)
+def ParseMetadata(x):
+    headerStart = -1
+    headerEnd = -1
 
-chapterMisc.sort()
+    for i in range(0, len(x)):
+        if(headerStart == -1 and x[i].startswith("---")):
+            headerStart = i
+        elif(headerEnd == -1 and x[i].startswith("---")):
+            headerEnd = i
 
-print(chapterMisc)
+    fileList = ParseBeatStructure(x[headerEnd+1:len(x)])
+
+    return ''.join(x[headerStart:headerEnd + 1]) + "\n\n", fileList
 
 # Handle Meta
 metadata = ""
+meta = ""
 concatChapters = ""
 
 with open("Meta.md", "r+") as f:
-    metadata = ''.join(f.readlines())
-    metadata += '\n\n'
+    meta = f.readlines()
 
-for chapter in chapterMisc:
+metadata, chapterOrder = ParseMetadata(meta)
+
+
+for chapter in chapterOrder:
     concatChapters += pypandoc.convert_file(chapter, "md")
     concatChapters += '\n\n'
 
