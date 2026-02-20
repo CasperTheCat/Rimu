3721778f155b03081a3ba24290c4b1c17e53c95f
Chris Anderson <11226374+CasperTheCat@users.noreply.github.com>
2026-02-21 01:28:51 +1300

    Updated Pathing

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
-FROM redrime/rime-compositor:1
-RUN mkdir /latex
-WORKDIR /latex
-ADD . /latex
-CMD ["python3", "Rimu.py"]
+FROM ubuntu:latest
+RUN apt update -y && apt upgrade -y && apt-get install -y texlive-lang-english texlive-luatex fonts-noto fonts-noto-cjk python3 pandoc python3-pip texlive-games texlive-humanities texlive texlive-latex-extra
+RUN apt install -y python3-venv
+RUN python3 -m venv /VirtEnv
+RUN /VirtEnv/bin/pip3 install pypandoc
+
+RUN mkdir /Rimu
+WORKDIR /Rimu
+ADD Rimu.py /Rimu
+CMD ["/VirtEnv/bin/python3", "/Rimu/Rimu.py", "/latex"]
--- Rimu.py
+++ Rimu.py
@@ -3,12 +3,18 @@ import sys
 import pypandoc
 import codecs
 
+RootPath = sys.argv[1]
+
+pandoc_extra_arguments = ['--verbose', f'--resource-path={RootPath}', '--embed-resources', '--standalone', '-f', 'markdown-implicit_figures']
+
+OutputDirectory = os.path.join(RootPath, "Output")
+
 chapterMisc = []
 
-for r,ds,fs in os.walk("./"):
+for r,ds,fs in os.walk(RootPath):
     for f in fs:
         if(f.startswith("Chapter")):
-            chapterMisc.append(r+f)
+            chapterMisc.append(os.path.join(r,f))
 
 print(chapterMisc)
 
@@ -20,22 +26,26 @@ print(chapterMisc)
 metadata = ""
 concatChapters = ""
 
-with open("Meta.md", "r+") as f:
+with open(os.path.join(RootPath, "Meta.md"), "r+") as f:
     metadata = ''.join(f.readlines())
     metadata += '\n\n'
 
 for chapter in chapterMisc:
-    concatChapters += pypandoc.convert_file(chapter, "md")
+    print("Processing: {}".format(chapter))
+    concatChapters += pypandoc.convert_file(chapter, "md", extra_args=pandoc_extra_arguments)
     concatChapters += '\n\n'
 
 temp = metadata + concatChapters
 
+if not os.path.exists(OutputDirectory):
+    os.makedirs(OutputDirectory)
+
 # Output intermediate
-with codecs.open("markdown.ric", "w+", "utf-8-sig") as f:
+with codecs.open(os.path.join(OutputDirectory, "markdown.ric"), "w+", "utf-8-sig") as f:
     f.write(temp)
 
 # Sanity the Tex File
-pypandoc.convert_text(temp, 'tex', format='md', outputfile='compile.tex', extra_args=['-s'])
+pypandoc.convert_text(temp, 'tex', format='md', outputfile=os.path.join(OutputDirectory, 'compile.tex'), extra_args=['-s'] + pandoc_extra_arguments)
 
 # Do the PDF
-pypandoc.convert_text(temp, 'pdf', format='md', outputfile='compile.pdf', extra_args=['--pdf-engine=lualatex'])
+pypandoc.convert_text(temp, 'pdf', format='md', outputfile=os.path.join(OutputDirectory, 'compile.pdf'), extra_args=['--pdf-engine=lualatex']  + pandoc_extra_arguments)
